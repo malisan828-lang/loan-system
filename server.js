f5e1a29643f74716429fa9869c2bf34ce9f5d073
malisan828-lang <malisan828@gmail.com>
2026-03-29 00:30:01 +0700

    fix server binding

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 require("dotenv").config();
 
 const express = require("express");
-const app = express();
+const http = require("http");
 
+const app = express();
+const server = http.createServer(app);
+
+// static
 app.use(express.static("public"));
 
+// test route
 app.get("/", (req, res) => {
     res.send("SERVER OK");
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, "0.0.0.0", () => {
+server.listen(PORT, "0.0.0.0", () => {
     console.log("Server running on port " + PORT);
 });
